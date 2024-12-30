e7daae6b463237e8ba64f18147f540f0aed62397
Brayden Chan <72172597+megabyte6@users.noreply.github.com>
2024-12-29 19:50:51 -0800

    Add project files

---
+++ src/client/java/com/github/megabyte6/thomas/ThomasDataGenerator.java
@@ -0,0 +1,11 @@
+package com.github.megabyte6.thomas;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ThomasDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
