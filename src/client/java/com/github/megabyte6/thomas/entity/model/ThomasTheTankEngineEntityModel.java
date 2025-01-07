398c786b1815e301372ad39209a630b40170afe8
Brayden Chan <72172597+megabyte6@users.noreply.github.com>
2025-01-06 22:39:57 -0800

    Fix model facing direction

--- src/client/java/com/github/megabyte6/thomas/entity/model/ThomasTheTankEngineEntityModel.java
+++ src/client/java/com/github/megabyte6/thomas/entity/model/ThomasTheTankEngineEntityModel.java
@@ -14,19 +14,23 @@ public class ThomasTheTankEngineEntityModel extends EntityModel<LivingEntityRend
         super(root);
     }
 
-	public static TexturedModelData getTexturedModelData() {
-		ModelData modelData = new ModelData();
-		ModelPartData modelPartData = modelData.getRoot();
-		ModelPartData bb_main = modelPartData.addChild("bb_main", ModelPartBuilder.create().uv(0, 28).cuboid(-5.0F, -5.0F, -8.5F, 2.0F, 5.0F, 5.0F, new Dilation(0.0F))
-		.uv(27, 23).cuboid(3.0F, -5.0F, -8.5F, 2.0F, 5.0F, 5.0F, new Dilation(0.0F))
-		.uv(32, 7).cuboid(3.0F, -3.0F, 4.5F, 2.0F, 3.0F, 3.0F, new Dilation(0.0F))
-		.uv(14, 28).cuboid(-5.0F, -3.0F, 4.5F, 2.0F, 3.0F, 3.0F, new Dilation(0.0F))
-		.uv(24, 0).cuboid(-2.0F, -11.5F, 3.0F, 4.0F, 3.0F, 4.0F, new Dilation(0.0F))
-		.uv(1, 40).cuboid(-3.0F, -8.0F, 10.0F, 6.0F, 6.0F, 1.0F, new Dilation(0.0F))
-		.uv(19, 36).cuboid(-3.0F, -11.5F, -10.0F, 6.0F, 3.0F, 10.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 24.0F, 0.0F));
+    public static TexturedModelData getTexturedModelData() {
+        ModelData modelData = new ModelData();
+        ModelPartData modelPartData = modelData.getRoot();
+        ModelPartData bb_main = modelPartData.addChild("bb_main", ModelPartBuilder.create(),
+                ModelTransform.pivot(0.0F, 24.0F, 0.0F));
+        bb_main.addChild("TopChonk_r1", ModelPartBuilder.create().uv(19, 36)
+                        .cuboid(-3.0F, -12.5F, -10.0F, 6.0F, 3.0F, 10.0F, new Dilation(0.0F)).uv(1, 40)
+                        .cuboid(-3.0F, -8.0F, 10.0F, 6.0F, 6.0F, 1.0F, new Dilation(0.0F)).uv(24, 0)
+                        .cuboid(-2.0F, -12.5F, 3.0F, 4.0F, 3.0F, 4.0F, new Dilation(0.0F)).uv(14, 28)
+                        .cuboid(-5.0F, -3.5F, 4.5F, 2.0F, 3.0F, 3.0F, new Dilation(0.0F)).uv(32, 7)
+                        .cuboid(3.0F, -3.5F, 4.5F, 2.0F, 3.0F, 3.0F, new Dilation(0.0F)).uv(27, 23)
+                        .cuboid(3.0F, -5.5F, -8.5F, 2.0F, 5.0F, 5.0F, new Dilation(0.0F)).uv(0, 28)
+                        .cuboid(-5.0F, -5.5F, -8.5F, 2.0F, 5.0F, 5.0F, new Dilation(0.0F)),
+                ModelTransform.of(0.0F, 0.0F, 0.0F, -3.1416F, 0.0F, 3.1416F));
         bb_main.addChild("Body_r1",
                 ModelPartBuilder.create().uv(0, 0).cuboid(-4.0F, -10.0F, -9.0F, 8.0F, 20.0F, 8.0F, new Dilation(0.0F)),
-                ModelTransform.of(0.0F, -10.0F, 0.0F, -1.5708F, 0.0F, 3.1416F));
+                ModelTransform.of(0.0F, -10.0F, 0.0F, 1.5708F, 0.0F, 0.0F));
         return TexturedModelData.of(modelData, 64, 64);
     }
 }
