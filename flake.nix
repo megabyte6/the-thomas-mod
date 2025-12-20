ddfb043349a58261687aebc9d1f75da947fc0f6e
Brayden Chan <72172597+megabyte6@users.noreply.github.com>
2025-12-20 02:06:08 -0800

    Add libraries required to run Minecraft

--- flake.nix
+++ flake.nix
@@ -4,11 +4,27 @@
   outputs = {nixpkgs, ...}: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
+    libs = [
+      pkgs.mesa
+      pkgs.libGL
+
+      pkgs.xorg.libX11
+      pkgs.xorg.libXrandr
+      pkgs.xorg.libXcursor
+      pkgs.xorg.libXi
+      pkgs.xorg.libXext
+      pkgs.xorg.libXxf86vm
+    ];
   in {
     devShells.${system}.default = pkgs.mkShell {
-      packages = [
-        pkgs.javaPackages.compiler.temurin-bin.jdk-25
-      ];
+      packages =
+        [
+          pkgs.javaPackages.compiler.temurin-bin.jdk-25
+        ]
+        ++ libs;
+      shellHook = ''
+        export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath libs}:$LD_LIBRARY_PATH"
+      '';
     };
   };
 }
