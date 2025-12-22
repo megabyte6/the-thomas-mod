73b5642f5a708b10aa0c38dd8772cdfa72256e01
Brayden Chan <72172597+megabyte6@users.noreply.github.com>
2025-12-21 18:15:04 -0800

    Use flake-utils for per-system devShells
    Add flake-utils input and create per-system devShells with
    flakeUtils.lib.eachDefaultSystem. Include temurin JDK 25, git, and extra
    libs (udev, pipewire, flite, xorg libs) and set JAVA_HOME and
    LD_LIBRARY_PATH

--- flake.nix
+++ flake.nix
@@ -1,30 +1,42 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
-  outputs = {nixpkgs, ...}: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {inherit system;};
-    libs = [
-      pkgs.mesa
-      pkgs.libGL
-
-      pkgs.xorg.libX11
-      pkgs.xorg.libXrandr
-      pkgs.xorg.libXcursor
-      pkgs.xorg.libXi
-      pkgs.xorg.libXext
-      pkgs.xorg.libXxf86vm
-    ];
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      packages =
-        [
-          pkgs.javaPackages.compiler.temurin-bin.jdk-25
-        ]
-        ++ libs;
-      shellHook = ''
-        export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath libs}:$LD_LIBRARY_PATH"
-      '';
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flakeUtils.url = "github:numtide/flake-utils";
   };
+
+  outputs = {
+    nixpkgs,
+    flakeUtils,
+    ...
+  }:
+    flakeUtils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+
+      jdk = pkgs.javaPackages.compiler.temurin-bin.jdk-25;
+      libs = with pkgs; [
+        mesa
+        libGL
+        udev
+        pipewire
+        flite
+
+        xorg.libX11
+        xorg.libXi
+        xorg.libXrandr
+        xorg.libXcursor
+      ];
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs;
+          [
+            jdk
+
+            git
+          ]
+          ++ libs;
+
+        env.JAVA_HOME = jdk;
+        env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath libs;
+      };
+    });
 }
